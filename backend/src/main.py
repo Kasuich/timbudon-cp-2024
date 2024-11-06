a8483a6ca3f8c7359ea96ab803e9fa06af5db94d
kasuich <kasui3108@gmail.com>
2024-11-06 22:09:40 +0300

    add fastapi

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,30 +5,41 @@ from pprint import pformat
 from time import perf_counter
 
 import grpc
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
 from inference_pb2 import InferenceReply, InferenceRequest
 from inference_pb2_grpc import InferenceServerStub
 from PIL import Image
 
-image = Image.open("cat.jpg")
-buffered = BytesIO()
-image.save(buffered, format="JPEG")
-image_bytes = buffered.getvalue()
+app = FastAPI()
 
 logging.basicConfig(level=logging.INFO)
 
 
-async def main():
-    async with grpc.aio.insecure_channel("[::]:50052 ") as channel:
+async def send_grpc_request(image_bytes):
+    async with grpc.aio.insecure_channel("[::]:50052") as channel:
         stub = InferenceServerStub(channel)
         start = perf_counter()
 
         res: InferenceReply = await stub.inference(
-            InferenceRequest(image=[image_bytes, image_bytes, image_bytes])
+            InferenceRequest(image=[image_bytes, image_bytes])
         )
         logging.info(
             f"pred = {pformat(res.pred)} in {(perf_counter() - start) * 1000:.2f}ms"
         )
+        return str(res.pred)
+
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    image = Image.open(file.file)
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    image_bytes = buffered.getvalue()
+
+    prediction = await send_grpc_request(image_bytes)
+    return {"prediction": prediction}
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    uvicorn.run(app, host="0.0.0.0", port=8000)
